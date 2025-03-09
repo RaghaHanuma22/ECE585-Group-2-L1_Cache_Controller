4996a6fccc25d4dec939a5b60e9a920cc76b6800
Raghavendra Davarapalli <ragha@mo.ece.pdx.edu>
2025-03-09 00:19:42 -0800

    Update 03/09

--- main/msd.sv
+++ main/msd.sv
@@ -5,52 +5,98 @@ import pkg_cache::*;
   bit [3:0] n;  // To store the leading 0
   logic [31:0] hex_value;   // To store the 8-digit hex value (32 bits)
   bit [13:0] set_id;
+  bit [1:0] d_victim_cache;
+  bit [1:0] i_victim_cache;
+  bit replacement_done;
 
-
-/*
-function mesi_state_t next_state(mesi_state_t current_state, );
-
-endfunction
-*/
 
 function void process_cache(bit [3:0] n);
 case (n)
-    0: begin //Read request to L1 data cache
+
+  0: begin //Read request to L1 data cache
+      increment_d_read();
       set_id = hex_value[19:6];
-      if(d_cache_check(d_cache,hex_value)) begin
-        $display("Cache hit for address %0h in way %0h",hex_value,d_way_idx);
+      
+      if(d_cache_check(d_cache, hex_value)) begin
+        // Cache hit case
+        $display("Cache hit for address %0h in way %0h", hex_value, d_way_idx);
+        d_LRU_Update(set_id, d_way_idx);
+        increment_d_hit();
+        
+        // Update MESI state based on current state
         if(d_cache[set_id][d_way_idx].current_state == I) begin
-        d_cache[set_id][d_way_idx].current_state = E;    //First cache hit comes from acting processor
+          d_cache[set_id][d_way_idx].current_state = E;  // First cache hit from acting processor
         end
         else if(d_cache[set_id][d_way_idx].current_state == M) begin
-          d_cache[set_id][d_way_idx].current_state = M;
+          d_cache[set_id][d_way_idx].current_state = M;  // Maintain Modified state
         end
         else begin
-          d_cache[set_id][d_way_idx].current_state = S;  //Considering that the 2nd cache hit comes from a different processor
+          d_cache[set_id][d_way_idx].current_state = S;  // Considering hit from different processor
         end
-        $display("Current state is %s!",d_cache[set_id][d_way_idx].current_state);
+        
+        $display("Current state is %s!", d_cache[set_id][d_way_idx].current_state);
       end
       else begin
-        for(int j=0;j<d_ways;j++) begin
-          if(d_cache[set_id][j].tag==0) begin
-          d_cache[set_id][j].tag=hex_value[31:20];
-          d_cache[set_id][j].current_state = E;
-          break;
-          end
-          else begin
-            $display("Cache Full da venna!!");
-            //function for LRU
+        // Cache miss case
+        increment_d_miss();
+        $display("Cache miss for address %0h, reading from L2 cache", hex_value);
+        
+        replacement_done = 0;
+        empty_flag = 0;
+        invalid_flag = 0;
+        
+        // Step 1: Check for empty slots (tag == 0)
+        for(int j = 0; j < d_ways; j++) begin
+          if(d_cache[set_id][j].tag == 0) begin
+            d_cache[set_id][j].tag = hex_value[31:20];
+            d_cache[set_id][j].current_state = E;
+            d_LRU_Update(set_id, j);
+            empty_flag = 1;
+            replacement_done = 1;
+            $display("Replaced in empty way %0d", j);
+            break;
           end
         end
-        $display("Now cache has %p",d_cache[set_id]);
-    end
-    end
+        
+        // Step 2: If no empty slots, check for invalid states
+        if(!empty_flag) begin
+          $display("Cache full da venna!");
+          for(int e = 0; e < d_ways; e++) begin
+            if(d_cache[set_id][e].current_state == I) begin
+              d_cache[set_id][e].tag = hex_value[31:20];
+              d_cache[set_id][e].current_state = E;
+              d_LRU_Update(set_id, e);
+              invalid_flag = 1;
+              replacement_done = 1;
+              $display("Replaced in invalid way %0d", e);
+              break;
+            end
+          end
+        end
+        
+        // Step 3: If no invalid states, use LRU victim
+        if(!empty_flag && !invalid_flag) begin
+          d_victim_cache = find_d_LRU_way(set_id);
+          d_cache[set_id][d_victim_cache].tag = hex_value[31:20];
+          d_cache[set_id][d_victim_cache].current_state = E;
+          d_LRU_Update(set_id, d_victim_cache);
+          $display("Replaced LRU victim way %0d", d_victim_cache);
+        end
+        
+        $display("Now cache has %p", d_cache[set_id]);
+      end
+  end
+
 
     1:begin //write request to L1 data cache
+      increment_write();
       set_id = hex_value[19:6];
       if(d_cache_check(d_cache,hex_value)) begin
         $display("Cache hit for address %0h in way %0h",hex_value,d_way_idx);
         $display("Data written at address %0h in way %0h",hex_value,d_way_idx);
+        increment_d_hit();
+        d_LRU_Update(set_id,d_way_idx);
+
         if(d_cache[set_id][d_way_idx].current_state == I) begin
         d_cache[set_id][d_way_idx].current_state = M;    //First cache hit comes from acting processor
         end
@@ -65,29 +111,69 @@ case (n)
         end
         $display("Current state is %s!",d_cache[set_id][d_way_idx].current_state);
       end
+
+      //cache miss case
       else begin
+        increment_d_miss();
+         $display("Cache miss for address %0h, reading from L2 cache", hex_value);
+        replacement_done = 0;
+        empty_flag = 0;
+        invalid_flag = 0;
+
+        // Step 1: Check for empty slots (tag == 0)
         for(int j=0;j<d_ways;j++) begin
           if(d_cache[set_id][j].tag==0) begin
           d_cache[set_id][j].tag=hex_value[31:20];
           if(d_cache [set_id][j].current_state == I) begin
-          $display("Data is also being written through to Main Memory");
+          $display("Data is also being written through to L2 Cache"); //write through for first miss
           end
+          d_LRU_Update(set_id,j);
           d_cache[set_id][j].current_state = M;
+          empty_flag = 1;
+          replacement_done =1;
+          $display("Replaced in empty way %0d", j);
           break;
           end
-          else begin
-            $display("Cache Full da venna!!");
-            //function for LRU
+        end
+
+
+        // Step 2: If no empty slots, check for invalid states
+        if(~empty_flag) begin
+          $display("Cache Full da venna!!");
+          for(int e = 0; e < d_ways; e++) begin
+            if(d_cache[set_id][e].current_state == I) begin
+              d_cache[set_id][e].tag = hex_value[31:20];
+              d_cache[set_id][e].current_state = M;
+              d_LRU_Update(set_id, e);
+              invalid_flag = 1;
+              replacement_done = 1;
+              $display("Replaced in invalid way %0d", e);
+              break;
+            end
           end
         end
-        
+
+       // Step 3: If no invalid states, use LRU victim
+        if(!empty_flag && !invalid_flag) begin
+          d_victim_cache = find_d_LRU_way(set_id);
+          d_cache[set_id][d_victim_cache].tag = hex_value[31:20];
+          d_cache[set_id][d_victim_cache].current_state = M;
+          d_LRU_Update(set_id, d_victim_cache);
+          $display("Replaced LRU victim way %0d", d_victim_cache);
+        end
+
+         $display("Now cache has %p",d_cache[set_id]);
     end
-        $display("Now cache has %p",d_cache[set_id]);
     end
 
     2:begin //Instruction fetch to L1 instructions cache
+      increment_i_read();
       set_id = hex_value[19:6];
       if(i_cache_check(i_cache,hex_value)) begin
+        $display("Cache hit for address %0h in way %0h",hex_value,i_way_idx);
+        i_LRU_Update(set_id,i_way_idx);
+        increment_i_hit();
+
         if(i_cache[set_id][i_way_idx].current_state == I) begin
         i_cache[set_id][i_way_idx].current_state = E;    //First cache hit comes from acting processor
         end
@@ -96,23 +182,61 @@ case (n)
         end
         $display("Current state is %s!",i_cache[set_id][i_way_idx].current_state);
       end
+
+//Cache miss case
       else begin
+        increment_i_miss();
+         $display("Cache miss for address %0h, reading from L2 cache", hex_value);
+
+        replacement_done = 0;
+        empty_flag = 0;
+        invalid_flag = 0;
+
+        // Step 1: Check for empty slots (tag == 0)
         for(int j=0;j<i_ways;j++) begin
           if(i_cache[set_id][j].tag==0) begin
           i_cache[set_id][j].tag=hex_value[31:20];
+          i_cache[set_id][j].current_state = E;
+          i_LRU_Update(set_id,j);
+          empty_flag = 1;
+          replacement_done = 1;
+          $display("Replaced in empty way %0d", j);
           break;
-          end
-          else begin
-            $display("Cache Full da venna!!");
+          end    
+        end
+
+        // Step 2: If no empty slots, check for invalid states
+        if(~empty_flag) begin
+           $display("Cache Full da venna!!");
+           for(int e = 0; e < i_ways; e++) begin
+            if(i_cache[set_id][e].current_state == I) begin
+              i_cache[set_id][e].tag = hex_value[31:20];
+              i_cache[set_id][e].current_state = E;
+              i_LRU_Update(set_id, e);
+              invalid_flag = 1;
+              replacement_done = 1;
+              $display("Replaced in invalid way %0d", e);
+              break;
+            end
           end
         end
-        //$display("Current state is %s!",current_state);
+
+      // Step 3: If no invalid states, use LRU victim
+        if(!empty_flag && !invalid_flag) begin
+          i_victim_cache = find_i_LRU_way(set_id);
+          i_cache[set_id][i_victim_cache].tag = hex_value[31:20];
+          i_cache[set_id][i_victim_cache].current_state = E;
+          i_LRU_Update(set_id, i_victim_cache);
+          $display("Replaced LRU victim way %0d", i_victim_cache);
+        end
+
         $display("Now cache has %p",i_cache[set_id]);
       end
     end
 
     3:begin
       set_id = hex_value[19:6];
+      $display("Invalid command from L2 Cache");
       if(d_cache_check(d_cache,hex_value)) begin
         d_cache[set_id][d_way_idx].current_state = I;
         $display("Now cache has %p",d_cache[set_id]);
@@ -123,6 +247,15 @@ case (n)
         /*
           RFO doubt: what happens if P1 is not in M state then where does P2 get its data from? 
         */
+        set_id = hex_value[19:6];
+        $display("RFO from another processor!");
+        if(d_cache_check(d_cache,hex_value)) begin
+          if(d_cache[set_id][d_way_idx].current_state == M)begin
+            $display("Modified data is being written to L2 Cache at address %0h",hex_value);
+          end
+            d_cache[set_id][d_way_idx].current_state = I;
+            $display("Our copy has become invalid: %p",d_cache[set_id]);
+        end
     end
 
     8:begin
@@ -153,7 +286,7 @@ case (n)
 
 
     default: begin
-      //current_state = I;
+      current_state = I;
     end
 
     
@@ -162,7 +295,7 @@ endfunction
 
   // Main execution
   initial begin
-    fd = $fopen("rwims.din", "r");
+    fd = $fopen("lru_trace.din", "r");
     if(fd)
       $display("File opened!!");
     else 
@@ -170,8 +303,10 @@ endfunction
     
     i_cache_init();
     d_cache_init();
+    
     while (!$feof(fd)) begin
       $fgets(line, fd);
+      
       if (line.len() > 0) begin
         // Extract the leading bit (first character)
         n = line.substr(0, 0).atoi();
@@ -188,7 +323,10 @@ endfunction
             $display("Failed to convert hex string: %s", hex_str);
           end
           process_cache(n);
-          break; //to remove
+          `ifdef debug
+            break;
+          `endif
+           //to remove
         end
 
       
@@ -199,7 +337,7 @@ endfunction
 
 
     end
-
+/*
   process_cache(0);
   #10;
   process_cache(1);
@@ -207,9 +345,10 @@ endfunction
   //process_cache(8);
   #10;
   //process_cache(9);
-  process_cache(3);
-
-    
+  //process_cache(3);
+  #10;
+  process_cache(4);
+*/
     $fclose(fd);
     $display("File operation done!");
   end
--- main/pkg_cache.sv
+++ main/pkg_cache.sv
@@ -6,10 +6,15 @@ parameter by_of = 512; //64 bytes so 512 bits
 parameter TAG_WIDTH = 12;
 bit [2:0] d_way_idx;
 bit [1:0] i_way_idx;
-int cache_hit;
-int cache_miss;
-int cache_read;
+int d_cache_hit;
+int d_cache_miss;
+int d_cache_read;
 int cache_write;
+int i_cache_hit;
+int i_cache_miss;
+int i_cache_read;
+bit empty_flag;
+bit invalid_flag;
 
 typedef enum bit [1:0] {M=2'b00,E=2'b01,S=2'b10,I=2'b11} mesi_state_t;
 
@@ -39,7 +44,7 @@ logic [13:0] set_idx = address[19:6];
 logic [11:0] tag = address[31:20];
 
 for(int i=0;i<i_ways;i++) begin 
-  if(i_cache_mem[set_idx][i].tag == tag) begin
+  if(i_cache_mem[set_idx][i].tag == tag && ~(i_cache_mem[set_idx][i].current_state == I)) begin
   hit=1;
   i_way_idx=i;
   break;
@@ -65,7 +70,7 @@ logic [13:0] set_idx = address[19:6];
 logic [11:0] tag = address[31:20];
 
 for(int i=0;i<d_ways;i++) begin 
-  if(d_cache_mem[set_idx][i].tag == tag) begin
+  if(d_cache_mem[set_idx][i].tag == tag && ~(d_cache_mem[set_idx][i].current_state == I)) begin
   hit=1;
   d_way_idx=i;
   break;
@@ -73,7 +78,7 @@ for(int i=0;i<d_ways;i++) begin
 end
 
 if(hit) begin
-  $display("Cache hit!");
+  //$display("Cache hit!");
   return 1;
 end
   else begin
@@ -90,6 +95,7 @@ function void i_cache_init();
     for(int j=0;j<i_ways;j++) begin
       i_cache [i][j] ='0;
       i_cache [i][j].current_state = I;
+      i_cache [i][j].lru_bits = 2'b00;
     end
   end
 endfunction
@@ -100,28 +106,107 @@ function void d_cache_init();
     for(int j=0;j<d_ways;j++) begin
       d_cache [i][j] ='0;
       d_cache [i][j].current_state = I;
+      d_cache [i][j].lru_bits = 3'b000;
     end
   end
 endfunction
 
+//////////////////////////LRU///////////////////////////////////////
 
 
-function int increment_hit();
-    cache_hit++;
+// LRU Update task for instruction cache - uses 2-bit LRU tracking
+task automatic i_LRU_Update(logic [13:0] set_idx, logic [1:0] way_idx);
+    logic [1:0] current_lru;
+    
+    // Store current LRU value of the accessed way
+    current_lru = i_cache[set_idx][way_idx].lru_bits;
+    
+    // Update LRU values for all ways in this set
+    for(int i = 0; i < i_ways; i++) begin
+        // If way's LRU value is greater than the accessed way's value, decrement it
+        if(i_cache[set_idx][i].lru_bits > current_lru)
+            i_cache[set_idx][i].lru_bits = i_cache[set_idx][i].lru_bits - 1;
+    end
+    
+    // Set the accessed way's LRU value to maximum (3 for 2-bit field)
+    i_cache[set_idx][way_idx].lru_bits = 2'b11;
+endtask
+
+// LRU Update task for data cache - uses 3-bit LRU tracking
+task automatic d_LRU_Update(logic [13:0] set_idx, logic [2:0] way_idx);
+    logic [2:0] current_lru;
+    
+    // Store current LRU value of the accessed way
+    current_lru = d_cache[set_idx][way_idx].lru_bits;
+    
+    // Update LRU values for all ways in this set
+    for(int i = 0; i < d_ways; i++) begin
+        // If way's LRU value is greater than the accessed way's value, decrement it
+        if(d_cache[set_idx][i].lru_bits > current_lru)
+            d_cache[set_idx][i].lru_bits = d_cache[set_idx][i].lru_bits - 1;
+    end
+    
+    // Set the accessed way's LRU value to maximum (7 for 3-bit field)
+    d_cache[set_idx][way_idx].lru_bits = 3'b111;
+endtask
+
+// Function to find LRU way for instruction cache
+function automatic logic [1:0] find_i_LRU_way(logic [13:0] set_idx);
+    for(int i = 0; i < i_ways; i++) begin
+        // Find way with LRU value of 0 (least recently used)
+        if(i_cache[set_idx][i].lru_bits == 0)
+            return i[1:0];
+    end
+    
+    // Fallback - should never reach here if LRU is properly maintained
+    return 0;
 endfunction
 
-function int increment_miss();
-    cache_miss++;
+// Function to find LRU way for data cache
+function automatic logic [2:0] find_d_LRU_way(logic [13:0] set_idx);
+    for(int i = 0; i < d_ways; i++) begin
+        // Find way with LRU value of 0 (least recently used)
+        if(d_cache[set_idx][i].lru_bits == 0)
+            return i[2:0];
+    end
+    
+    // Fallback - should never reach here if LRU is properly maintained
+    return 0;
 endfunction
 
-function int increment_read();
-    cache_read++;
+
+
+
+///////////////////////////////////////////////////////////////////
+
+
+
+function int increment_d_hit();
+    return d_cache_hit++;
+endfunction
+
+function int increment_d_miss();
+    return d_cache_miss++;
+endfunction
+
+function int increment_d_read();
+    return d_cache_read++;
 endfunction
 
 function int increment_write();
-    cache_write++;
+    return cache_write++;
 endfunction
 
+function int increment_i_hit();
+    return i_cache_hit++;
+endfunction
 
+function int increment_i_miss();
+    return i_cache_miss++;
+endfunction
+
+function int increment_i_read();
+    return i_cache_read++;
+endfunction
 
 endpackage
